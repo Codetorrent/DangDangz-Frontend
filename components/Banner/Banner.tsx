32d6b0e29097fe53d8f9edb4810c2dfb6cee4f90
swon95 <shimtkddnjs13@gmail.com>
2023-06-04 02:48:43 +0900

    [FEAT] Banner & Styled-Components

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,7 +1,58 @@
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
 import React from "react";
 
 const Banner = () => {
-    return <div>Banner</div>;
+    return (
+        <BannerWrapper>
+            <BannerView>
+                <Title>Discover, collect, and sell extraordinary NFTs</Title>
+                <SubTitle>
+                    on the world&apos;s first & largest NFT marketplace
+                </SubTitle>
+                <ButtonView>
+                    <BannerButton variant="contained" size="large">
+                        Explore
+                    </BannerButton>
+                    <BannerButton variant="outlined" size="large">
+                        Create
+                    </BannerButton>
+                </ButtonView>
+            </BannerView>
+        </BannerWrapper>
+    );
 };
 
 export default Banner;
+
+const BannerWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 100px;
+`;
+
+const BannerView = styled.div`
+    max-width: 600px;
+`;
+
+const Title = styled.div`
+    font-size: 48px;
+    font-weight: 700;
+`;
+
+const SubTitle = styled.div`
+    margin-top: 24px;
+    font-size: 20px;
+    font-weight: 700;
+    max-width: 350px;
+`;
+
+const ButtonView = styled.div`
+    margin-top: 24px;
+`;
+
+const BannerButton = styled(Button)`
+    margin-right: 16px;
+    padding: 12px 48px;
+`;
